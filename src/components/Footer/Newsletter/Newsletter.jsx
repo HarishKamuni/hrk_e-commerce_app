8f2728e1050ccf0e2aa99af261da191d13c10fd3
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2024-03-23 13:03:36 +0530

    final

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,51 +1,64 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-    FaFacebookF,
-    FaTwitter,
-    FaInstagram,
-    FaLinkedinIn,
-} from "react-icons/fa";
-import "./Newsletter.scss";
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import './Newsletter.scss';
+import { NavLink } from 'react-router-dom';
 const Newsletter = () => {
-    const [email,setEmail] = useState("")
-    const submitHandler =(e)=>{
-        e.preventDefault();
-        if(email !== "") alert(`Your Email:${email} Successfully Subscribe!!!`);
-        else alert('please fill the field!!!')
-    }
-    return (
-        <div className="newsletter-section">
-            <div className="newsletter-content">
-                <span className="small-text">Newsletter</span>
-                <span className="big-text">
-                    Sign up for latest updates and offers
-                </span>
-                <div className="form" >
-                    <form onSubmit={submitHandler} className="d-flex"> 
-                    <input type="email" placeholder="Email Address" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
-                    <button type="submit">Subscribe</button>
-                    </form>
-                </div>
-                <span className="text">
-                    Will be used in accordance with our Privacy Policy
-                </span>
-                <span className="social-icons">
-                    <div className="icon">
-                        <FaLinkedinIn size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaFacebookF size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaTwitter size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaInstagram size={14} />
-                    </div>
-                </span>
-            </div>
+  const [email, setEmail] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (email !== '') alert(`Your Email : ${email} Successfully Subscribe!!!`);
+    else alert('please fill the field!!!');
+  };
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest updates and offers</span>
+        <div className="form">
+          <form onSubmit={submitHandler} className="d-flex">
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <button type="submit">Subscribe</button>
+          </form>
         </div>
-    );
+        <span className="text">
+          Will be used in accordance with our Privacy Policy
+        </span>
+        <span className="social-icons">
+          <div className="icon">
+            <NavLink to="/">
+              <FaLinkedinIn size={14} style={{ color: '#fff' }} />
+            </NavLink>
+          </div>
+          <div className="icon">
+            <NavLink to="/">
+              <FaFacebookF size={14} style={{ color: '#fff' }} />
+            </NavLink>
+          </div>
+          <div className="icon">
+            <NavLink to="https://twitter.com/HarishKamuni21">
+              <FaTwitter size={14} style={{ color: '#fff' }} />
+            </NavLink>
+          </div>
+          <div className="icon">
+            <NavLink to="/">
+              <FaInstagram size={14} style={{ color: '#fff' }} />
+            </NavLink>
+          </div>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
